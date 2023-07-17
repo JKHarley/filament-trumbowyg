8f509c5afee1f496d2ba1f58e9b4c85880faa639
JKHarley <JKHarley@users.noreply.github.com>
2023-07-17 12:13:31 +0000

    Fix styling

--- src/Trumbowyg.php
+++ src/Trumbowyg.php
@@ -3,8 +3,8 @@
 namespace JKHarley\FilamentTrumbowyg;
 
 use Filament\Forms\Components\Concerns\HasPlaceholder;
-use JKHarley\FilamentTrumbowyg\traits\HasChangeActiveDropdownIcon;
 use JKHarley\FilamentTrumbowyg\traits\HasButtons;
+use JKHarley\FilamentTrumbowyg\traits\HasChangeActiveDropdownIcon;
 use JKHarley\FilamentTrumbowyg\traits\HasTagClasses;
 
 class Trumbowyg extends \Filament\Forms\Components\Field
