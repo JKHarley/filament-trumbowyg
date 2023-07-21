66f48d700bee5addb7ff72439a13fff9888c382d
James Harley <27085725+JKHarley@users.noreply.github.com>
2023-07-21 19:16:18 +0100

    Add support for link targets (#20)

--- src/Trumbowyg.php
+++ src/Trumbowyg.php
@@ -5,6 +5,7 @@ namespace JKHarley\FilamentTrumbowyg;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
 use JKHarley\FilamentTrumbowyg\traits\HasButtons;
 use JKHarley\FilamentTrumbowyg\traits\HasChangeActiveDropdownIcon;
+use JKHarley\FilamentTrumbowyg\traits\HasLinkTargets;
 use JKHarley\FilamentTrumbowyg\traits\HasMinimalLinks;
 use JKHarley\FilamentTrumbowyg\traits\HasRemoveFormatPasted;
 use JKHarley\FilamentTrumbowyg\traits\HasSemantic;
@@ -25,6 +26,7 @@ class Trumbowyg extends \Filament\Forms\Components\Field
     use HasTagsToKeep;
     use HasUrlProtocol;
     use HasMinimalLinks;
+    use HasLinkTargets;
 
     protected string $view = 'filament-trumbowyg::trumbowyg';
 }
